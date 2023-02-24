f9212d6e91cc0df9908606491ea04c0f78a0bd24
KOTESHWARA RAO PAKEERI <87428991+Koteshwr@users.noreply.github.com>
2023-02-24 06:41:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let mainpage = document.getElementById("mainpage");
+let loginpage = document.getElementById("loginpage");
+let sbtn = document.getElementById("signinbtn");
+let plus = document.getElementById("plus");
+
+let qns = Array.from(document.getElementsByClassName("faq-card-ans"))
+let ans = Array.from(document.getElementsByClassName("faq-card-qn"))
+var qnno;
+
+for (i in qns) {
+    qns[i].style.visibility = "hidden"
+    qns[i].style.height = "0px"
+}
+let reveal = (j) => {
+    if (qns[j].style.visibility == "hidden") {
+        qns[j].style.visibility = "visible";
+        qns[j].style.height = "auto"
+    }
+    else {
+    qns[j].style.height = "0px"
+    qns[j].style.visibility = "hidden";
+    }
+}
+let closeother = (i) => {
+    for (j in qns) {
+        if (j != i) {
+    qns[j].style.height = "0px"
+    qns[j].style.visibility = "hidden"
+
+        }
+    }
+}
+
+ans.forEach(element => {
+    element.addEventListener('click', () => {
+        reveal(element.id)
+        closeother(element.id)
+    })
+});
+
+
+function openSigninPage() {
+    sbtn.style.display = "none"
+    mainpage.style.display = "none"
+    loginpage.style.display = "contents"
+}
+
+
+qn.addEventListener('click', () => {
+    if (faq.style.visibility == "hidden") {
+        faq.style.visibility = "visible";
+    }
+    else {
+        faq.style.visibility = "hidden";
+    }
+})
+
+let main = document.getElementById("main");
+var l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for (i in l) {
+    let el = document.createElement("div");
+    el.classList.add("box");
+    main.appendChild(el);
+};
